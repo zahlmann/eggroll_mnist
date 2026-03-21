61af0e032c2f1012682d9c1bfd17ff4b0dc7a2a5
Johann Zahlmann <johann.zahlmann@gmail.com>
2026-03-21 21:37:36 +0100

    fully unroll inner scan (10 iterations); remove XLA autotune flag

--- mnist_eggroll_optimized.py
+++ mnist_eggroll_optimized.py
@@ -1,5 +1,4 @@
 import os
-os.environ["XLA_FLAGS"] = os.environ.get("XLA_FLAGS", "") + " --xla_gpu_autotune_level=4"
 import sys
 import time
 import argparse
@@ -132,7 +131,7 @@ def train_epoch(w1, w2, w3, X_batched, y_batched, sigma, lr, key):
 
         inner_carry = (base1, w2_f, w3_f, sigma_f, y_one_hot)
         scan_data = (A1_c, xB1_T_c, A2_c, B2_c, A3_c, B3_c)
-        _, (fitness_pos, fitness_neg) = jax.lax.scan(chunk_forward, inner_carry, scan_data)
+        _, (fitness_pos, fitness_neg) = jax.lax.scan(chunk_forward, inner_carry, scan_data, unroll=N_CHUNKS)
 
         fitness_pos = fitness_pos.reshape(-1)
         fitness_neg = fitness_neg.reshape(-1)
